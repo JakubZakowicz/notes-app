c5f9423100e3fe5e480612391367959a6c4949b4
Jakub Zakowicz <j.zakowicz99@gmail.com>
2022-05-15 16:27:13 +0200

    add register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
+import Register from './pages/Register'
 import Notes from './pages/Notes'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Notes />} />
         <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
         <Route path="/" element={<Navigate replace to="/auth/login" />} />
       </Routes>
     </Router>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -4,23 +4,29 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 
 interface LoginInputs {
+  username: string,
   email: string;
   password: string;
+  confirmPassword: string
 }
 
 const schema = Yup.object({
+  username: Yup.string().required('Username is required!').min(3).max(48),
   email: Yup.string()
     .email('Invalid email!')
     .required('Email address is required!'),
   password: Yup.string().required('Password is required!'),
+  confirmPassword: Yup.string()
+     .oneOf([Yup.ref('password'), null], 'Passwords must match!')
 });
 
-const Login = () => {
+const Register = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<LoginInputs>({
+    reValidateMode: 'onSubmit',
     resolver: yupResolver(schema),
   });
   const onSubmit: SubmitHandler<LoginInputs> = data => console.log(data);
@@ -28,9 +34,18 @@ const Login = () => {
     <div className="container mx-auto">
       <div className="flex justify-center h-screen items-center">
         <div className="bg-white border w-96 rounded-xl px-10 py-5 shadow-lg">
-          <h1 className="text-3xl font-semibold text-center">Login</h1>
+          <h1 className="text-3xl font-semibold text-center">Register</h1>
           <form onSubmit={handleSubmit(onSubmit)} className="mt-5">
             <div className="flex flex-col gap-8">
+              <div className="relative">
+                <label htmlFor="email">Username</label>
+                <input
+                  id="username"
+                  {...register('username')}
+                  className="h-10 border rounded-lg w-full pl-2"
+                />
+                <p className="text-red-500 absolute">{errors.username?.message}</p>
+              </div>
               <div className="relative">
                 <label htmlFor="email">Email address</label>
                 <input
@@ -51,28 +66,31 @@ const Login = () => {
                 />
                 <p className="text-red-500 absolute">{errors.password?.message}</p>
               </div>
-            </div>
-            <div className="flex justify-between mt-7">
-              <div className="flex items-center gap-2">
-                <input type="checkbox" className="w-4 h-4" />
-                <p>Remember me</p>
+              <div className="relative">
+                <label htmlFor="password">Confirm password</label>
+                <input
+                  id="password"
+                  type="password"
+                  {...register('confirmPassword')}
+                  className="h-10 border rounded-lg w-full pl-2"
+                />
+                <p className="text-red-500 absolute">{errors.confirmPassword?.message}</p>
               </div>
-              <p>Forgot Password?</p>
             </div>
-            <button className="bg-yellow-500 w-full mt-5 py-2 rounded-lg text-white hover:bg-yellow-600">
-              Login
+            <button className="bg-yellow-500 w-full mt-10 py-2 rounded-lg text-white hover:bg-yellow-600">
+              Register
             </button>
           </form>
           <p className="text-center mt-5">
-            Don't have an account?
-            <Link to="/auth/register" className="text-yellow-600 ml-1">
-              Sign up here
+            Already have an account?
+            <Link to="/auth/login" className="text-yellow-600 ml-1">
+              Sign in here
             </Link>
           </p>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default Login;
+export default Register
