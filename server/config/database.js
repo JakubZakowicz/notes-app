399fece6afccef39121833c0923c7f896a4c1c5a
JakubZakowicz <j.zakowicz99@gmail.com>
2023-06-13 13:29:20 +0200

    change database settings

--- server/config/database.js
+++ server/config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'password'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: true
+      ssl: false
     },
     debug: false,
   },
